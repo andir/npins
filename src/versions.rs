fe8e6fe06c17529de7dcf67c71ca43c7adc6a50c
Janne Heß <janne@hess.ooo>
2022-08-31 12:20:43 +0200

    Bump format version to 3

--- src/versions.rs
+++ src/versions.rs
@@ -5,7 +5,7 @@ use anyhow::{Context, Result};
 use serde_json::{json, Map, Value};
 
 /// The current format version
-pub const LATEST: u64 = 2;
+pub const LATEST: u64 = 3;
 
 /// Custom manual deserialize wrapper that checks the version
 pub fn from_value_versioned(value: Value) -> Result<NixPins> {
@@ -77,6 +77,9 @@ pub fn upgrade(mut pins_raw: Map<String, Value>) -> Result<Value> {
             log::info!("There is nothing to do");
         },
         2 => {
+            log::info!("There is nothing to do");
+        },
+        3 => {
             log::info!("sources.json is already up to date");
         },
         unknown => {
