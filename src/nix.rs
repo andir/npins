b6cefb51441c97c177f3d4702f4553ad5496d661
piegames <git@piegames.de>
2025-05-13 13:52:54 +0200

    check all URLs afterwards
    This is a fixup of ddb925e70846e41ca74b6038a346d5fbdfecae6b: "check all
    URLs beforehand".
    
    The issue with that approach is that git server implementations are the
    wild west, and that there is no single and good way to check for the
    liveness of a git repo given its URL. Doing the check before all
    operations had the crucial downside that any false positives would
    needlessly break npins for that repository.
    
    So instead, the check has been moved to after the actual operations, and
    is only triggered on the failure path. This still leads to the same
    improvement of error messages, while being guaranteed free of false
    positives.

--- src/nix.rs
+++ src/nix.rs
@@ -1,7 +1,6 @@
-use crate::check_url;
+use crate::{check_git_url, check_url};
 use anyhow::{Context, Result};
 use data_encoding::BASE64;
-use log::debug;
 
 #[allow(unused)]
 pub struct PrefetchInfo {
@@ -21,38 +20,36 @@ pub fn hash_to_sri(s: &str, algo: &str) -> Result<String> {
 
 pub async fn nix_prefetch_tarball(url: impl AsRef<str>) -> Result<String> {
     let url = url.as_ref();
-    check_url(url).await?;
+    let result = async {
+        log::debug!(
+            "Executing `nix-prefetch-url --unpack --name source --type sha256 {}`",
+            url
+        );
+        let output = tokio::process::Command::new("nix-prefetch-url")
+            .arg("--unpack") // force calculation of the unpacked NAR hash
+            .arg("--name")
+            .arg("source") // use the same symbolic store path name as `builtins.fetchTarball` to avoid downloading the source twice
+            .arg("--type")
+            .arg("sha256")
+            .arg(url)
+            .output()
+            .await
+            .with_context(|| format!("Failed to spawn nix-prefetch-url for {}", url))?;
 
-    log::debug!(
-        "Executing `nix-prefetch-url --unpack --name source --type sha256 {}`",
-        url
-    );
-    let output = tokio::process::Command::new("nix-prefetch-url")
-        .arg("--unpack") // force calculation of the unpacked NAR hash
-        .arg("--name")
-        .arg("source") // use the same symbolic store path name as `builtins.fetchTarball` to avoid downloading the source twice
-        .arg("--type")
-        .arg("sha256")
-        .arg(url)
-        .output()
-        .await
-        .with_context(|| format!("Failed to spawn nix-prefetch-url for {}", url))?;
+        // FIXME: handle errors and pipe stderr through
+        if !output.status.success() {
+            return Err(anyhow::anyhow!(format!(
+                "failed to prefetch url: {}\n{}",
+                url,
+                String::from_utf8_lossy(&output.stderr)
+            )));
+        }
 
-    // FIXME: handle errors and pipe stderr through
-    if !output.status.success() {
-        return Err(anyhow::anyhow!(format!(
-            "failed to prefetch url: {}\n{}",
-            url,
-            String::from_utf8_lossy(&output.stderr)
-        )));
-    }
-
-    let stdout = String::from_utf8_lossy(&output.stdout);
-    let hash = stdout.trim();
-
-    log::debug!("Got sha256: {}", hash);
-
-    hash_to_sri(&hash, "sha256")
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        log::debug!("Got hash: {}", stdout);
+        hash_to_sri(&stdout.trim(), "sha256")
+    };
+    check_url(result.await, url).await
 }
 
 pub async fn nix_prefetch_git(
@@ -61,69 +58,70 @@ pub async fn nix_prefetch_git(
     submodules: bool,
 ) -> Result<String> {
     let url = url.as_ref();
-    check_url(url).await?;
 
-    log::debug!(
-        "Executing: `nix-prefetch-git {}{} {}`",
-        if submodules {
-            "--fetch-submodules "
-        } else {
-            ""
-        },
-        url,
-        git_ref.as_ref()
-    );
-    let mut output = tokio::process::Command::new("nix-prefetch-git");
-    if submodules {
-        output.arg("--fetch-submodules");
-    }
-    let output = output
-        // Disable any interactive login attempts, failing gracefully instead
-        .env("GIT_TERMINAL_PROMPT", "0")
-        .env("GIT_SSH_COMMAND", "ssh -o StrictHostKeyChecking=yes")
-        .arg(url)
-        .arg(git_ref.as_ref())
-        .output()
-        .await
-        .with_context(|| {
-            format!(
-                "Failed to spawn nix-prefetch-git for {} @ {}",
-                url,
-                git_ref.as_ref()
-            )
-        })?;
-
-    // FIXME: handle errors and pipe stderr through
-    if !output.status.success() {
-        return Err(anyhow::anyhow!(format!(
-            "failed to prefetch url: {}\n{}",
+    let result = async {
+        log::debug!(
+            "Executing: `nix-prefetch-git {}{} {}`",
+            if submodules {
+                "--fetch-submodules "
+            } else {
+                ""
+            },
             url,
-            String::from_utf8_lossy(&output.stderr)
-        )));
-    }
+            git_ref.as_ref()
+        );
+        let mut output = tokio::process::Command::new("nix-prefetch-git");
+        if submodules {
+            output.arg("--fetch-submodules");
+        }
+        let output = output
+            // Disable any interactive login attempts, failing gracefully instead
+            .env("GIT_TERMINAL_PROMPT", "0")
+            .env("GIT_SSH_COMMAND", "ssh -o StrictHostKeyChecking=yes")
+            .arg(url)
+            .arg(git_ref.as_ref())
+            .output()
+            .await
+            .with_context(|| {
+                format!(
+                    "Failed to spawn nix-prefetch-git for {} @ {}",
+                    url,
+                    git_ref.as_ref()
+                )
+            })?;
 
-    #[allow(unused)]
-    #[derive(Debug, serde::Deserialize)]
-    struct NixPrefetchGitResponse {
-        url: String,
-        rev: String,
-        date: String,
-        path: String,
-        sha256: String,
-        #[serde(rename = "fetchSubmodules")]
-        fetch_submodules: bool,
-        #[serde(rename = "deepClone")]
-        deep_clone: bool,
-        #[serde(rename = "leaveDotGit")]
-        leave_dot_git: bool,
-    }
+        // FIXME: handle errors and pipe stderr through
+        if !output.status.success() {
+            return Err(anyhow::anyhow!(format!(
+                "failed to prefetch url: {}\n{}",
+                url,
+                String::from_utf8_lossy(&output.stderr)
+            )));
+        }
 
-    debug!(
-        "nix-prefetch-git output: {}",
-        String::from_utf8_lossy(&output.stdout)
-    );
-    let info: NixPrefetchGitResponse = serde_json::from_slice(&output.stdout)
-        .context("Failed to deserialize nix-pfetch-git JSON response.")?;
+        #[allow(unused)]
+        #[derive(Debug, serde::Deserialize)]
+        struct NixPrefetchGitResponse {
+            url: String,
+            rev: String,
+            date: String,
+            path: String,
+            sha256: String,
+            #[serde(rename = "fetchSubmodules")]
+            fetch_submodules: bool,
+            #[serde(rename = "deepClone")]
+            deep_clone: bool,
+            #[serde(rename = "leaveDotGit")]
+            leave_dot_git: bool,
+        }
 
-    hash_to_sri(&info.sha256, "sha256")
+        log::debug!(
+            "nix-prefetch-git output: {}",
+            String::from_utf8_lossy(&output.stdout)
+        );
+        let info: NixPrefetchGitResponse = serde_json::from_slice(&output.stdout)
+            .context("Failed to deserialize nix-pfetch-git JSON response.")?;
+        hash_to_sri(&info.sha256, "sha256")
+    };
+    check_git_url(result.await, url).await
 }
